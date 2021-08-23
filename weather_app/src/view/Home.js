13215371cf02b7cfaa580b2a57332b21d76bc35e
juj624 <julienpenot@outlook.com>
2021-08-23 14:19:55 +0200

    create displayWeather

--- weather_app/src/view/Home.js
+++ weather_app/src/view/Home.js
@@ -1,12 +1,40 @@
 import { useState, useEffect } from "react";
-// import Button from "../components/Button";
+import DisplayWeather from "../components/DisplayWeather";
+
 
 function Home() {
 
+    const APIKEY = "9c269693b4b9a53b83d8c1c2dfa761d2";
+
+
     const [weather, setWeather] = useState({})
+
+    // function asynchrone effectuer pour l'api
+
+
+    async function weatherData(e) {
+
+        e.preventDefault()
+        if (weather.city === "") {
+            alert("Add values")
+        } else {
+            const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${weather.city},${weather.country}&appid=${APIKEY}`)
+                .then((res) => res.json());
+
+            setWeather({
+
+                data: data,
+            });
+        }
+    }
+
+
+    // getPosts();
+
 
 
     const handleChange = (e) => {
+
         const name = e.target.name;
         const value = e.target.value;
 
@@ -19,25 +47,24 @@ function Home() {
         // console.log(weather.city, weather.country)
     };
 
-
-    // useEffect(() => {
-    //     fetch(`api.openweathermap.org/data/2.5/weather?q={city name}&appid=9c269693b4b9a53b83d8c1c2dfa761d2`)
-    //         .then((res) => res.json())
-    //         .then((res) => setWeather(res));
-    // }, []);
-
-
     return (
         <div>
-            {/* <Button /> */}
             <form className="form-button">
                 <label> Enter the city name and country :</label>
                 <input className="input" type="text" name="city" placeholder="city" onChange={e => handleChange(e)} />
                 <input className="input" type="text" name="country" placeholder="country" onChange={e => handleChange(e)} />
-                <button className="button" type="button">Search</button>
+                <button className="button" type="button" onClick={(e) => weatherData(e)}>Search</button>
             </form>
+
+            {
+                weather.data !== undefined ? (
+                    <div>
+                        <DisplayWeather data={weather.data} />
+                    </div>
+                ) : null}
         </div>
     );
 }
+
 
 export default Home;
